eeb529fb0df512f77d8cd61cb0e8c0b09b91a919
Olga <cloud4olga@gmail.com>
2023-08-07 13:11:58 -0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+// 456 -> 5; 782 -> 8; 918 -> 1;
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int a = (number - (number / 100 *100) - (number % 10)) / 10;
+Console.WriteLine(a);
+
